51e3179ed9d8d288ba1a40892c4c4ca109cf2366
Noah Yakubu <49097877+Noahabbey1@users.noreply.github.com>
2025-07-04 22:35:02 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -18,5 +18,7 @@ with app.app_context(): # configuration you shd add so that sqlalchemy will do i
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000) # the debug true helps us have a better debugging in our console 
 
-
+@app.route("/")
+def home():
+    return "Flask backend is running!"
 

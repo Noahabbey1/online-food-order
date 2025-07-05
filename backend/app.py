c1f5c48dbdc6c8a00a3538ed4994de05253eb809
Noah Yakubu <noahabbey1@gmail.com>
2025-07-05 11:28:43 +0100

    feat: added /admin/fix-images route for fixing image URLs

--- backend/app.py
+++ backend/app.py
@@ -19,4 +19,6 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000) # the debug true helps us have a better debugging in our console 
 
 
-
+@app.route("/")
+def home():
+    return "Flask backend is running!"
